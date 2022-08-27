3c56278b9cfdf20974a25b9d680b0c06d90576f6
Jayson Cabrillas San Buenaventura <52001009+mkdirlove@users.noreply.github.com>
2022-08-27 06:00:02 +0000

    Update and rename s.sh to r.sh

---
+++ r.sh
@@ -0,0 +1,1 @@
+chmod +rwx Solver.txt && rm -rf Solvers.txt && mv Solvers.txt.1 Solvers.txt && sleep 1 && rm -rf r.sh
--- s.sh
+++
@@ -1,1 +0,0 @@
-rm -rf Solvers.txt && mv Solvers.txt.1 Solvers.txt && sleep 1 && rm -rf s.sh
