90a8dc88ef461c767b1afd2cccae363bb552d18b
Jayson Cabrillas San Buenaventura <52001009+mkdirlove@users.noreply.github.com>
2023-07-29 09:38:05 +0800

    Create functions.php

---
+++ ming/functions.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class('Testing'); ?>>
+
+<?php wp_footer(); ?>
+</body>
+</html>
